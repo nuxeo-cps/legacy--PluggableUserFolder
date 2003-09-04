1e6fd9b1a55f8fbb6b4a966abc084be3547c1f72
lregebro <devnull@localhost>
2003-09-04 14:51:17 +0000

    Adding the CMF/CPS skins

---
+++ skins/localgroup_apply.py
@@ -0,0 +1,5 @@
+##parameters=apply_groups
+
+context.manage_groupRolesApplyGroups(apply_groups)
+
+return context.localgroup_manage()
---
+++ skins/localgroup_unapply.py
@@ -0,0 +1,5 @@
+##parameters=unapply_groups
+
+context.manage_groupRolesUnapplyGroups(unapply_groups)
+
+return context.localgroup_manage()
