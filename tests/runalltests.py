f1f953013d17679a0c8069bbfa1ff651e60d3b4a
lregebro <devnull@localhost>
2004-01-18 10:50:57 +0000

    Added the automatic SOFTWARE_HOME finding here too. Guess it could be done with a bat-file instead, for windows.

--- tests/runalltests.py
+++ tests/runalltests.py
@@ -9,6 +9,28 @@
 #
 
 import os, sys
+
+if not 'SOFTWARE_HOME' in os.environ.keys():
+    # Find the root of the Zope install.
+    curdir = os.getcwd()
+    zope_home = None
+    look_foor_file = 'z2.py' # This may change in Zope2.7
+    while not zope_home:
+        if look_foor_file in os.listdir(curdir):
+            zope_home = curdir
+            continue
+        if curdir == os.path.split(curdir)[0]:
+            break
+        curdir, ignore = os.path.split(curdir)
+
+    if zope_home:
+        zope_home = os.path.join(zope_home, 'lib', 'python')
+        print "Setting SOFTWARE_HOME to", zope_home
+        os.environ['SOFTWARE_HOME'] = zope_home
+    else:
+        print "SOFTWARE_HOME not set, and can not be found."
+        sys.exit(-1)
+
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
 
